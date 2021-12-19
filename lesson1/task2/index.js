b6c3d41ad24e4ba1d5792238e7aecf20937d19d0
arybary <l3fljl5ldydy@gmail.com>
2021-12-19 12:05:08 +0200

    react

---
+++ lesson1/task2/index.js
@@ -0,0 +1,12 @@
+const rootElement = document.querySelector("#root");
+
+const greetingElem = React.createElement("div", { className: "greeting" }, [
+  React.createElement("div", { className: "greeting__title" }, "Hello, world!"),
+
+  React.createElement(
+    "div",
+    { className: "greeting__text" },
+    "I'm learning React"
+  ),
+]);
+ReactDOM.render(greetingElem, rootElement);
