df5858c8775f7660ef7204f76eefd4f4731123ca
arybary <l3fljl5ldydy@gmail.com>
2021-12-19 14:54:30 +0200

    nnh

---
+++ lesson1/task3/webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'review_build')},
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
