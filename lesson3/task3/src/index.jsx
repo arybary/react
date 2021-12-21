0d4699d7ba8bf9ade86eed4cd07da8f271e7b647
arybary <l3fljl5ldydy@gmail.com>
2021-12-21 14:45:45 +0200

    k3t3

--- lesson3/task3/src/index.jsx
+++ lesson3/task3/src/index.jsx
@@ -10,6 +10,6 @@ const userInfo = {
     "https://avatars.githubusercontent.com/u/53406649?s=400&u=f8795d7bb075d808c3e3db199423b99729392675&v=4",
 };
 ReactDOM.render(
-  <Comment user={userInfo} text="Good job!" date={new Date("2021")} />,
+  <Comment author={userInfo} text="Good job!" date={new Date("2021")} />,
   rootElement
 );
