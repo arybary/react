dece72b8c6032b6358b5e8741c2cc5b229dbb6ca
arybary <l3fljl5ldydy@gmail.com>
2021-12-21 18:44:35 +0200

    l3t5

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import moment from "moment";
 
+const formatDate = (date) => moment(date).format("DD MMM YY");
+
 export default (props) => {
-  const  date = moment(props.user.birthDate).format("DD MMM YY");
- 
   return (
     <div className="profile">
-      <div className="profile__name"> {`${props.user.firstName} ${props.user.lastName}`}</div>
-      <div className="profile__birth"> {`Was born ${date} in ${props.user.birthPlace}`}</div>     
+      <div className="profile__name">
+        {" "}
+        {`${props.user.firstName} ${props.user.lastName}`}
+      </div>
+      <div className="profile__birth">
+        {" "}
+        {`Was born ${formatDate(props.user.birthDate)} in ${props.user.birthPlace}`}
+      </div>
     </div>
   );
 };
