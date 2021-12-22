78cac9aceee4dadfd7fe7a699124f6a7128c275c
arybary <l3fljl5ldydy@gmail.com>
2021-12-22 22:06:53 +0200

    l5t5

--- lesson5/task5/src/Toggler.jsx
+++ lesson5/task5/src/Toggler.jsx
@@ -1,23 +1,23 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Toggler extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: "Off",
+      counter: 'Off',
     };
   }
 
   onButton = (e) => {
-    const click = e.target.textContent === "Off";
+    const click = e.target.textContent === 'Off';
     click
-      ? this.setState({ counter: "On" })
-      : this.setState({ counter: "Off" });
+      ? this.setState({ counter: 'On' })
+      : this.setState({ counter: 'Off' });
   };
 
   render() {
     return (
-      <button className="counter__button" onClick={this.onButton}>
+      <button className='counter__button' onClick={this.onButton}>
         {this.state.counter}
       </button>
     );
