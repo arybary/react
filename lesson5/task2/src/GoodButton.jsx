0ba40527fef6562fb3592f62799f15c2b0e696cc
arybary <l3fljl5ldydy@gmail.com>
2021-12-22 20:08:49 +0200

    l5t2

---
+++ lesson5/task2/src/GoodButton.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class GoodButton extends Component {
+  heanderClick(e){
+    alert(e.target.textContent)
+  }
+  render() {
+    return (
+      <button className="fancy-button" onClick={this.heanderClick}>      
+        Click me!
+      </button>
+    );
+  }
+}
+
+export default GoodButton;
