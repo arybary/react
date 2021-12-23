c2fcae71f6d341a358d4e7d8535ed7ffecebd5d2
arybary <l3fljl5ldydy@gmail.com>
2021-12-23 19:00:45 +0200

    l7t3

--- lesson7/task3/src/TransactionsList.jsx
+++ lesson7/task3/src/TransactionsList.jsx
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import Transaction from "./Transaction.jsx";
 
-class TransactionsList extends Component {
-  render() {
-    return (
-      <ul className="transactions">
-        {this.props.transactions.map((trans) => (
-          <Transaction key={trans.id} {...trans} />
-        ))}
-      </ul>
-    );
-  }
-}
+const TransactionsList = ({ transactions }) => {
+  return (
+    <ul className="transactions">
+      {transactions.map((transaction) => (
+        <Transaction key={transaction.id} {...transaction} />
+      ))}
+    </ul>
+  );
+};
 
 export default TransactionsList;
