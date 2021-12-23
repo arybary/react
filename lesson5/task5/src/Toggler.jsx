702ed79980405e103909ade2ed6c2a9475748eca
arybary <l3fljl5ldydy@gmail.com>
2021-12-23 10:44:15 +0200

    l6t6

--- lesson5/task5/src/Toggler.jsx
+++ lesson5/task5/src/Toggler.jsx
@@ -4,21 +4,20 @@ class Toggler extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 'Off',
+      active: false,
     };
   }
 
-  onButton = (e) => {
-    const click = e.target.textContent === 'Off';
-    click
-      ? this.setState({ counter: 'On' })
-      : this.setState({ counter: 'Off' });
+  toggle = () => {
+    this.setState({
+      active: !this.state.active,
+    });
   };
 
   render() {
     return (
-      <button className='toggler' onClick={this.onButton}>
-        {this.state.counter}
+      <button className='toggler' onClick={this.toggle}>
+        {this.state.active ? 'On' : 'Off'}
       </button>
     );
   }
