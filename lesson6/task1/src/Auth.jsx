fdd525aeee3379a1a0b150da6a191ee8976dcb7f
arybary <l3fljl5ldydy@gmail.com>
2021-12-23 13:00:30 +0200

    l6t1

---
+++ lesson6/task1/src/Auth.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Greeting from "./Greeting.jsx";
+import Login from "./Login.jsx";
+import Logout from "./Logout.jsx";
+
+class Auth extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
+  onLogin = () => {
+    this.setState({ isLoggedIn: true });
+  };
+
+  onLogout = () => {
+    this.setState({ isLoggedIn: false });
+  };
+
+  render() {
+    return (
+      <div className="panel">
+        <Greeting isLoggedIn={this.state.isLoggedIn} />
+        {this.state.isLoggedIn ? (
+          <Logout onClick={this.onLogout} />
+        ) : (
+          <Login onClick={this.onLogin} />
+        )}
+      </div>
+    );
+  }
+}
+
+export default Auth;
---
+++ lesson6/task1/src/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = (props) => {
+ 
+  return (
+    <button className="login" onClick={props.onClick}>
+      Login
+    </button>
+  );
+};
+export default Login;
