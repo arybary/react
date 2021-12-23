484fe77e226957159fca595b6c27bae42966535b
arybary <l3fljl5ldydy@gmail.com>
2021-12-23 18:20:57 +0200

    l7t3

---
+++ lesson7/task3/src/TransactionsList.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Transaction from "./Transaction.jsx";
+
+const TransactionsList = ({ transactions }) => {
+  return (
+    <ul className="transactions">
+      {transactions.map((trans) => (
+        <Transaction key={trans.id} {...trans} />
+      ))}
+    </ul>
+  );
+};
+
+export default TransactionsList;
