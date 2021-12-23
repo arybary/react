66c7112a65c45197434fa84761cde707993fa041
arybary <l3fljl5ldydy@gmail.com>
2021-12-23 10:50:59 +0200

    color

--- lesson5/task6/src/ColorPicker.jsx
+++ lesson5/task6/src/ColorPicker.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 class ColorPicker extends Component {
-  setBodyColor = (color) => {
+  setNameColor = (color) => {
     const enterElem = document.querySelector(".picker__title");
     enterElem.textContent = color;
   };
@@ -15,18 +15,18 @@ class ColorPicker extends Component {
       <div>
           <button
             className="picker__button picker__button_coral"
-            onMouseEnter={() => this.setBodyColor("Coral")}
-            onMouseLeave={() => this.setBodyColor("")}
+            onMouseEnter={() => this.setNameColor("Coral")}
+            onMouseLeave={() => this.setNameColor("")}
           ></button>
           <button
             className="picker__button picker__button_aqua"
-            onMouseEnter={() => this.setBodyColor("Aqua")}
-            onMouseLeave={() => this.setBodyColor("")}
+            onMouseEnter={() => this.setNameColor("Aqua")}
+            onMouseLeave={() => this.setNameColor("")}
           ></button>
           <button
             className="picker__button picker__button_bisque"
-            onMouseEnter={() => this.setBodyColor("Bisque")}
-            onMouseLeave={() => this.setBodyColor("")}
+            onMouseEnter={() => this.setNameColor("Bisque")}
+            onMouseLeave={() => this.setNameColor("")}
           ></button>
         </div>
       </div>
