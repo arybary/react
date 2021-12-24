5f4e7b6d2816f70e5b7420798c665c0a5fcdaef7
arybary <l3fljl5ldydy@gmail.com>
2021-12-24 17:56:41 +0200

    l8t

--- lesson8/task3/src/Dimensions.jsx
+++ lesson8/task3/src/Dimensions.jsx
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 
 class Dimensions extends Component {
-  constructor(props) {
-    super(props);
-    const { innerWidth, innerHeight } = window;
-    this.state = { width: innerWidth, height: innerHeight };
-    document.title=`${innerWidth} x ${innerHeight}`
-  }
+  state = { width: null, height: null };
 
   componentDidMount() {
     window.addEventListener("resize", this.onResize);
+    const { innerWidth, innerHeight } = window;
+    this.setDimensions(innerWidth, innerHeight);
   }
   componentWillUnmount() {
     window.removeEventListener("resize", this.onResize);
   }
   onResize = (e) => {
     const { innerWidth, innerHeight } = e.target;
-    this.setState({ width: innerWidth, height: innerHeight });
-    document.title=`${innerWidth} x ${innerHeight}`
+    this.setDimensions(innerWidth, innerHeight);
+  };
+  setDimensions = (width, height) => {
+    this.setState({ width, height });
+    document.title = `${width} x ${height}`;
   };
   render() {
     return (
