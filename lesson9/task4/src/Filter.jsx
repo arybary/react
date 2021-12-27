99647c95c6b8de7618d4e318ff07cac7a537a4c2
arybary <l3fljl5ldydy@gmail.com>
2021-12-27 18:42:46 +0200

    l9t4

--- lesson9/task4/src/Filter.jsx
+++ lesson9/task4/src/Filter.jsx
@@ -1,29 +1,45 @@
 import React, { Component } from "react";
+import User from "./User.jsx";
 
 class Filter extends Component {
   state = {
     filterText: "",
+    list: this.props.users,
+    count: this.props.users.length
   };
 
   onChange = (e) => {
-    e.preventDefault();
-
+    e.preventDefault();  
+    const filterList = this.props.users.filter(
+      (user) => user.name === e.target.value
+    );
     this.setState({
       filterText: e.target.value,
+      list: filterList,
+      count:filterList.length
     });
-    this.props.updateData(this.state.filterText);
+    
   };
 
   render() {
+    let newList;
+    newList = this.state.filterText === "" ? this.props.users : this.state.list;
     return (
-      <div className="filter">
-        <span className="filter__count">{this.props.count}</span>
-        <input
-          type="text"
-          className="filter__input"
-          onChange={this.onChange}
-          value={this.state.filterText}
-        />
+      <div>
+        <div className="filter">
+          <span className="filter__count">{newList.length}</span>
+          <input
+            type="text"
+            className="filter__input"
+            onChange={this.onChange}
+            value={this.state.filterText}
+          />
+        </div>
+        <ul className="users">
+          {newList.map((user) => (
+            <User key={user.id} {...user} />
+          ))}
+        </ul>
       </div>
     );
   }
--- lesson9/task4/src/UsersList.jsx
+++ lesson9/task4/src/UsersList.jsx
@@ -4,30 +4,12 @@ import Filter from "./Filter.jsx";
 
 class UsersList extends Component {
   state = {
-    filterText: ""
+    users: this.props.users,
   };
-  updateData = (value) => {
-    this.setState({ filterText: value});
-  };
-
   render() {
-    console.log(this.state.filterText);
-    let newList;
-    newList =
-      this.state.filterText===''
-        ? this.props.users
-        : this.props.users.filter(
-            (user) => user.name === this.state.filterText
-          );
-
     return (
       <div>
-        <Filter updateData={this.updateData} count={newList.length}/>
-        <ul className="users">
-          {newList.map((user) => (
-            <User key={user.id} {...user} />
-          ))}
-        </ul>
+        <Filter users={this.state.users} />;
       </div>
     );
   }
