e859c86c92d9052ccff604bcfde4534cb6b1a080
arybary <l3fljl5ldydy@gmail.com>
2021-12-27 17:29:48 +0200

    l9t4

--- lesson9/task4/src/UsersList.jsx
+++ lesson9/task4/src/UsersList.jsx
@@ -3,10 +3,33 @@ import User from "./User.jsx";
 import Filter from "./Filter.jsx";
 
 class UsersList extends Component {
-  render() {
-    console.log(this.props.users);
+  state = {
+    filterText: ""
+  };
+  updateData = (value) => {
+    this.setState({ filterText: value});
+  };
 
-    return <Filter users={this.props.users} />;
+  render() {
+    console.log(this.state.filterText);
+    let newList;
+    newList =
+      this.state.filterText===''
+        ? this.props.users
+        : this.props.users.filter(
+            (user) => user.name === this.state.filterText
+          );
+
+    return (
+      <div>
+        <Filter updateData={this.updateData} count={newList.length}/>
+        <ul className="users">
+          {newList.map((user) => (
+            <User key={user.id} {...user} />
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
