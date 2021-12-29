662d7f874f656f908df4fd274bd3430120bb4e72
arybary <l3fljl5ldydy@gmail.com>
2021-12-29 19:55:19 +0200

    l11t3

--- lesson11/task3/src/Expand.jsx
+++ lesson11/task3/src/Expand.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 
-
 const Expand = ({ children, title, isContent, toggleContent }) => {
-  let status = isContent
-    ? { content: children, chevron: "fas fa-chevron-up" }
-    : { content: '', chevron: "fas fa-chevron-down" };
   return (
     <div className="expand border">
       <div className="expand__header">
         <span className="expand__title">{title}</span>
         <button className="expand__toggle-btn" onClick={toggleContent}>
-        <i className={status.chevron} />
+          <i
+            className={isContent ? "fas fa-chevron-up" : "fas fa-chevron-down"}
+          />
         </button>
       </div>
-      <div className="expand__content">{status.content}</div>
+      <div className="expand__content">{isContent ? children : ""}</div>
     </div>
   );
 };
