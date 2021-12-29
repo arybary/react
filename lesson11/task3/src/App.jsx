bb27997ffbab5b8e14e35159ec8137b8ad2564f5
arybary <l3fljl5ldydy@gmail.com>
2021-12-29 19:38:05 +0200

    l13t3

--- lesson11/task3/src/App.jsx
+++ lesson11/task3/src/App.jsx
@@ -14,10 +14,10 @@ class App extends Component {
     return (
       <div className="app">
         <Expand title="Some title" isContent={this.state.isContent} toggleContent={this.toggleContent}>
-          <p>
-            Hooks are a new addition in React 16.8. They let you use state and
-            other React features without writing a class.
-          </p>
+        <p>
+        Hooks are a new addition in React 16.8. They let you use state and other React features
+        without writing a class.
+      </p>
         </Expand>
       </div>
     );
