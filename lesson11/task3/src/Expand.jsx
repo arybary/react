ca66848d9cba8ad5406ffac41b1e5c29b4ba9c80
arybary <l3fljl5ldydy@gmail.com>
2021-12-29 19:03:46 +0200

    l11t3

--- lesson11/task3/src/Expand.jsx
+++ lesson11/task3/src/Expand.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 const Expand = ({ children, title, isContent, toggleContent }) => {
   let status = !isContent
-    ? { content: null, chevron: faChevronDown }
-    : { content: children, chevron: faChevronUp };
+    ? { content: null, chevron: "fas fa-chevron-down" }
+    : { content: children, chevron: "fas fa-chevron-up" };
   return (
     <div className="expand border">
       <div className="expand__header">
         <span className="expand__title">{title}</span>
         <button className="expand__toggle-btn" onClick={toggleContent}>
-          <FontAwesomeIcon icon={status.chevron} />
+        <i className={status.chevron} />
         </button>
       </div>
       <div className="expand__content">{status.content}</div>
