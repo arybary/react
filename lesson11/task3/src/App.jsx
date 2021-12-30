14497be2d276833fb0e9f1d06bf61a0baab70ff1
arybary <l3fljl5ldydy@gmail.com>
2021-12-30 10:02:40 +0200

    l13t3

--- lesson11/task3/src/App.jsx
+++ lesson11/task3/src/App.jsx
@@ -2,18 +2,11 @@ import React, { Component } from "react";
 import Expand from "./Expand.jsx";
 
 class App extends Component {
-  state = {
-    isExpended: false,    
-  };
-  toggleContent = () => {
-    this.setState({ isContent: !this.state.isContent });
-  };
- 
 
   render() {
     return (
       <div className="app">
-        <Expand title="Some title" isExpanded={this.state.isContent} toggleContent={this.toggleContent}>
+        <Expand title="Some title">
         <p>
         Hooks are a new addition in React 16.8. They let you use state and other React features
         without writing a class.
--- lesson11/task3/src/Expand.jsx
+++ lesson11/task3/src/Expand.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Expand = ({ children, title, isExpanded, toggleContent }) => {
+
+class Expand extends Component {
+  state = {
+    isExpanded: false,
+
+  };
+  toggleContent = () => {
+    this.setState({isExpanded: !this.state.isExpanded });
+  };
+render(){
   return (
     <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">{title}</span>
-        <button className="expand__toggle-btn" onClick={toggleContent}>
-            {isExpanded ? (
+    <div className="expand__header">
+      <span className="expand__title">{this.props.title}</span>
+      <button className="expand__toggle-btn" onClick={this.toggleContent}>
+            {this.state.isExpanded ? (
               <i className="fas fa-chevron-up" />
             ) : (
               <i className="fas fa-chevron-down" />
             )}
           </button>
-      </div>
-      <div className="expand__content">{isExpanded ? children : ""}</div>
     </div>
+    <div className="expand__content">
+      {this.state.isExpanded ? this.props.children : ''}
+    </div>
+  </div>
   );
-};
-
+};}
 export default Expand;
