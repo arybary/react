084e2e279017e6734761e4bb7c298b1672d281d4
arybary <l3fljl5ldydy@gmail.com>
2021-12-30 18:21:01 +0200

    l12t1

--- lesson12/task1/src/Task.jsx
+++ lesson12/task1/src/Task.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const Task = ({ text, done }) => {
-  const listItemClasses = `list-item ${done ? "list-item_done __text" : ""}`;
+  const listItemClasses = `list-item ${done ? "list-item_done" : ""}`;
   return (
     <li className={listItemClasses}>
       <input
         type="checkbox"
         className="list-item__checkbox"
         defaultChecked={done}
-      />
-      {text}
+      /> <span className="list-item__text">{text}</span>
+     
       <button className="list-item__delete-btn"></button>
     </li>
   );
