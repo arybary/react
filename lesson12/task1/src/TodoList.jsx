997efa24a579ce27d88164f83c7fa109c383561e
arybary <l3fljl5ldydy@gmail.com>
2021-12-30 18:29:30 +0200

    l12t

--- lesson12/task1/src/TodoList.jsx
+++ lesson12/task1/src/TodoList.jsx
@@ -3,24 +3,7 @@ import CreateTaskInput from "./CreateTaskInput";
 import TasksList from "./TasksList";
 
 class TodoList extends Component {
-  state = {
-    task: "",
-  };
-
-  handleChange = (e) => {
-    this.setState({
-      task: e.target.value,
-    });
-  };
-  createTasks = () => {
-    fetch("https://61cdc8267067f600179c5c46.mockapi.io/tasks", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify({ text: this.state.task, done: true }),
-    });
-  };
+ 
   render() {
     return (
       <>
