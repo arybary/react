9668900d85362dbf0a05bb629344464c3b44cbb3
arybary <l3fljl5ldydy@gmail.com>
2022-01-12 10:27:13 +0200

    l34t3

--- live/12.01/index.js
+++ live/12.01/index.js
@@ -1,21 +1,18 @@
 const baseUrl = "https://61cdc8267067f600179c5c46.mockapi.io/obj";
 
-const createUser = (user) =>
-  fetch(baseUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(user),
-  });
-
 const inputElem = document.querySelector("form");
 
 const onCreateUser = (event) => {
   event.preventDefault();
   const userObj = Object.fromEntries(new FormData(inputElem));
 
-  return createUser(userObj)
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(userObj),
+  })
     .then((respone) => {
       return respone.json();
     })
